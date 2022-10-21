3d83ed3ad6703c39354f0e00ed611e6e8dc44415
inesdelatorre <ines@anotherplaceproductions.com>
2022-10-21 14:04:14 +0200

    Ethereum & JS tests

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-window.ethereum.request({method:'eth_requestAccounts'})
+import "regenerator-runtime/runtime";
+
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
+
+ethereumButton.addEventListener('click', () => {
+    getAccount();
+});
+
+async function getAccount() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    //alert(accounts);
+    const account = accounts[0];
+    showAccount.innerHTML = account;
+}
